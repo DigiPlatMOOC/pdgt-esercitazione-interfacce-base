da5492eda43bc981fe2c8350122c87a24a56954e
petrello <tommasop01@gmail.com>
2022-04-27 19:12:24 +0200

    Create http.c

---
+++ 4-socket/http.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "socket-lib.h"
+
+#define HOST "127.0.0.1"
+#define PORT 1234
+
+int main() {
+
+    if(socket_init() != 0) {
+        fprintf(stderr, "Socket initialization failed\n");
+        return 1;
+    }
+
+    SOCKET_T socket = socket_create();
+    if(socket == 0) {
+        fprintf(stderr, "Failed to create socket\n");
+        return 1;
+    }
+
+    if(socket_connect(socket, HOST, PORT) < 0) {
+        fprintf(stderr, "Failed to connect socket\n");
+        return 1;
+    }
+
+    printf("Connected to the socket %d\n", socket);
+
+}
